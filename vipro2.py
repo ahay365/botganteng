61e5f8c5ab9dd9f4a0040853511481c47290ba79
zerozeven <39113087+zerozeven@users.noreply.github.com>
2018-05-21 14:27:42 +0700

    Update vipro2.py

--- vipro2.py
+++ vipro2.py
@@ -11,7 +11,7 @@ from gtts import gTTS
 import time,random,sys,json,codecs,threading,glob,urllib,urllib2,urllib3,re,ast,os,subprocess,requests,tempfile
 
 vipro = LINETCR.LINE()
-vipro.login(token='Et6NCZ2qU6jNDSretd10.uFNiiDNnqAPm9Svxp3Sdqa.Xebmda0y3SJ4F3HdXdIFiRuWB9cMFHKYYGZ3jRKA07s=')
+vipro.login(token='EpJgyNvzK8xQp88I04t9.2PnOnQMgNRHUYkUzkK6jEq./p92mOc1BPmafZPSgTcw4mkpBeT/czsWHBLRbK7Aewc=')
 vipro.loginResult()
 
 print "Vipro-Login Success\n\n=====[Sukses Login]====="
